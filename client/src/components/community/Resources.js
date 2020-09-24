433b2e63272a1a63d2d47eb458c0c54d197d9e47
DavidChovanetz <dchovanetz@gmail.com>
2020-09-23 20:58:15 -0500

    resources page progress

--- client/src/components/community/Resources.js
+++ client/src/components/community/Resources.js
@@ -15,50 +15,51 @@ import suicidePreventionAction from '../../assets/suicidePreventionAction.jpg';
 function Resources() {
     return(
         <div className="resourcesMain">
+          <h1 class='resourcesTitle'><b>Resources for Suicide Prevention</b></h1><br/>
 <Row className="justify-content-md-center">    
-<CardDeck className="resourcesCardDeck align-items-center">
+<CardDeck className="resourcesCardDeck align-items-center  text-center">
 <Col xs={1} lg={12}></Col>
-  <Card>
-    <Card.Img variant="top" className="cardImg" src={suicidePrevention}/>
+  <Card className="resourcesCard hovereffect">
+    <Card.Img  variant="top" className="cardImg"  src={suicidePrevention}/>
     <Card.Body>
-      <Card.Title> <a href="https://texassuicideprevention.org/"> Texas Mental Health and Substance Use Services </a></Card.Title>
+      <Card.Title> <a href="https://texassuicideprevention.org/" class="stretched-link img-responsive"> Texas Mental Health and Substance Use Services </a></Card.Title>
     </Card.Body>
   </Card>
-  <Card>
+  <Card className="resourcesCard hovereffect">
     <Card.Img variant="top" className="cardImg" src={texasMentalHealth} />
     <Card.Body>
-      <Card.Title><a href="https://hhs.texas.gov/services/mental-health-substance-use"> Texas Mental Health and Substance Use Services </a></Card.Title>
+      <Card.Title><a href="https://hhs.texas.gov/services/mental-health-substance-use" class="stretched-link img-responsive"> Texas Mental Health and Substance Use Services </a></Card.Title>
 
     </Card.Body>
   </Card>
-  <Card>
+  <Card className="resourcesCard hovereffect">
     <Card.Img variant="top" className="cardImg" src={oklahomaYouthSuicidePrevention} />
     <Card.Body>
-      <Card.Title><a href="#">Oklahoma Youth Suicide Prevention Initiative: </a></Card.Title>
+      <Card.Title><a href="#" class="stretched-link img-responsive">Oklahoma Youth Suicide Prevention Initiative </a></Card.Title>
 
     </Card.Body>
   </Card>
   </CardDeck>
-  <CardDeck className="resourcesCardDeck">
-  <Col xs={1} lg={12}></Col>
-  <Card>
+  <CardDeck className="resourcesCardDeck align-items-center  text-center">
+  <Col xs={1} s={2} lg={12}></Col>
+  <Card className="resourcesCard hovereffect">
     <Card.Img variant="top" className="cardImg" src={preventTeenSuicide} />
     <Card.Body>
-      <Card.Title> <a href="https://www.sptsusa.org/">The Society to Prevent Teen Suicide:</a> </Card.Title>
+      <Card.Title> <a href="https://www.sptsusa.org/" class="stretched-link img-responsive">The Society to Prevent Teen Suicide</a> </Card.Title>
       
     </Card.Body>
   </Card>
-  <Card>
+  <Card className="resourcesCard hovereffect">
     <Card.Img variant="top" className="cardImg" src={suicidePreventionResourceCenter} />
     <Card.Body>
-      <Card.Title> <a href="http://www.sprc.org/ ">Suicide Prevention Resource Center : </a> </Card.Title>
+      <Card.Title> <a href="http://www.sprc.org/ " class="stretched-link img-responsive">Suicide Prevention Resource Center</a> </Card.Title>
 
     </Card.Body>
   </Card>
-  <Card>
+  <Card className="resourcesCard hovereffect">
     <Card.Img variant="top" className="cardImg" src={suicidePreventionAction}  />
     <Card.Body>
-      <Card.Title><a href="http://www.spanusa.org/"> Suicide Prevention Action Network USA:  </a> </Card.Title>
+      <Card.Title><a href="http://www.spanusa.org/" class="stretched-link img-responsive"> Suicide Prevention Action Network USA</a> </Card.Title>
 
     </Card.Body>
   </Card>
