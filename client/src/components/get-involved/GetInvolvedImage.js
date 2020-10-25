12dca4aab00c8ebe3783ca89498f11cb1e7e0f5c
Monica Silva <msilva78617@Monicas-MacBook-Air.local>
2020-10-25 12:00:11 -0500

    -push before making about us and get involved responsive

--- client/src/components/get-involved/GetInvolvedImage.js
+++ client/src/components/get-involved/GetInvolvedImage.js
@@ -9,10 +9,10 @@ function GetInvolvedImage() {
     return (
       <div>
         <Card>
-          <Card.Img className="darker" height={580} src={image}  />
+          <Card.Img className='darker c-image resize d-block w-100' height={580} src={image}  />
           <Card.ImgOverlay style={{ padding: '7rem' }}>
-            <Card.Text className='mt-5 carousel-center carousel-text'>
-            “I recently presented with Pam Boyd at our annual Suicide Prevention & RX Drug Awareness Summit and believe she is doing important and innovative work that cannot be ignored. Pam is passionate about preventing the unnecessary loss of life and is using her creativity to launch a new approach to suicide prevention.”       
+            <Card.Text className='mt-5 carousel-text'>
+            <span id='g-image-text'>“I recently presented with Pam Boyd at our annual Suicide Prevention & RX Drug Awareness Summit and believe she is doing important and innovative work that cannot be ignored. Pam is passionate about preventing the unnecessary loss of life and is using her creativity to launch a new approach to suicide prevention.”</span>       
             <p className="carousel-text d-flex justify-content-end">
             -M’liss Jenkins, Washington County Wellness Initiative (CHIO) 
             </p>             
